713d543150f10c98835f4968205c8a7053937bc3
humalajames13-collab <humala.james13@gmail.com>
2026-01-16 09:37:12 -0500

    Add files via upload

---
+++ style.js
@@ -0,0 +1,51 @@
+console.log("js console");
+
+var data;
+var grid = document.querySelector(".grid-container");
+
+var xhttp = new XMLHttpRequest();
+
+xhttp.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+
+data = JSON.parse(xhttp.responseText);
+console.log(data);
+
+data.forEach (function(movies){
+    let card = document.createElement("div");
+    card.classList.add("card");
+
+    let textData =
+    "<div class='movie-title'>" + movies.title + "</div>" +
+    "<span>" +
+    "cast:" + movies.cast + "<br>" + 
+    "Needs Research:" +
+    "</span>";
+    
+    card.innerHTML = textData;
+
+    if(movies.imgSrc){
+        card.style.backgroundImage = "url("+ movies.imgSrc +")";
+    }
+    grid.appendChild(card);
+});
+
+}
+};
+
+xhttp.open("GET", "movies.json",true);
+xhttp.send();
+let genresHTML = movies.genres
+  ? movies.genres.map(g => `<span class="genre">${g}</span>`).join("")
+  : "";
+
+let textData = `
+  <div class="card-content">
+    <h3 class="movie-title">${movies.title}</h3>
+    <p class="movie-year">${movies.year}</p>
+    <div class="genres">${genresHTML}</div>
+    <p class="movie-cast">${movies.cast.slice(0, 3).join(", ")}...</p>
+  </div>
+`;
+
+card.innerHTML = textData;
