56e194a7e468cc317d2d89f3f83261ee07868139
humalajames13-collab <humala.james13@gmail.com>
2026-02-13 09:44:31 -0500

    Add files via upload

--- style.js
+++ style.js
@@ -1,67 +1,77 @@
-console.log("js console");
-
-var data;
 var grid = document.querySelector(".grid-container");
 
-var xhttp = new XMLHttpRequest();
 
-xhttp.onreadystatechange = function(){
-    if(this.readyState == 4 && this.status == 200){
-
-data = JSON.parse(xhttp.responseText);
-console.log(data);
-
-data.forEach (function(movies){
-    let card = document.createElement("div");
-    card.classList.add("card");
-
-    let textData =
-    "<div class='movie-title'>" + movies.title + "</div>" +
-    "<span>" +
-    "cast:" + movies.cast + "<br>" + 
-    "Needs Research:" +
-    "</span>";
-    
-    card.innerHTML = textData;
-
-    if(movies.imgSrc){
-        card.style.backgroundImage = "url("+ movies.imgSrc +")";
-    }
-    grid.appendChild(card);
-});
-
+if (localStorage.getItem("movies")) {
+  var localData = JSON.parse(localStorage.getItem("movies"));
+  localData.forEach(function(movie) {
+    createCard(movie);
+  });
 }
+
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    var data = JSON.parse(xhttp.responseText);
+    data.forEach(function(movie) {
+      createCard(movie);
+    });
+  }
 };
-
-xhttp.open("GET", "movies.json",true);
+xhttp.open("GET", "movies.json", true);
 xhttp.send();
-let genresHTML = movies.genres
-  ? movies.genres.map(g => `<span class="genre">${g}</span>`).join("")
-  : "";
 
-let textData = `
-  <div class="card-content">
-    <h3 class="movie-title">${movies.title}</h3>
-    <p class="movie-year">${movies.year}</p>
-    <div class="genres">${genresHTML}</div>
-    <p class="movie-cast">${movies.cast.slice(0, 3).join(", ")}...</p>
-  </div>
-`;
-FormData.addEventListener("submit",function(e){
-  e.preventDefault();
-  let title = titleInput.value;
-  let publisher = devInput.value;
-  let releaseDate = releaseDataInput.value;
-  let gifSrc = gifInput.value;
-  let imgSrc = imgInput.value;
-  let newObj = {
-    "id":getNextId(),
-    "title": title,
-    "publisher":publisher,
-    "releaseDate":releaseDate,
-    "imgSrc":imgSrc,
-    "giftSrc":gifSrc};
-    submitData(newObj);
-    FormData.reset();
+
+function createCard(movie) {
+  let card = document.createElement("div");
+  card.classList.add("card");
+
+  
+  let movieImg = movie.thumbnail || movie.imgSrc || "";
+  
+  let genresHTML = movie.genres 
+    ? movie.genres.map(g => `<span class="genre">${g}</span>`).join("") 
+    : "";
+
+  let castText = movie.cast && movie.cast.length > 0 
+    ? movie.cast.slice(0, 3).join(", ") 
+    : "N/A";
+
+  card.innerHTML = `
+    <div class="card-image" style="background-image: url('${movieImg}')"></div>
+    <div class="card-content">
+      <div class='movie-title'>${movie.title}</div>
+      <div class='movie-year'>${movie.year}</div>
+      <div class="genres">${genresHTML}</div>
+      <div class='movie-cast'>Cast: ${castText}</div>
+    </div>
+  `;
+  
+  grid.appendChild(card);
+}
+  
+  const form = document.getElementById("movieForm");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const movie = {
+      title: document.getElementById("titleInput").value,
+      year: document.getElementById("yearInput").value,
+      thumbnail: document.getElementById("imgInput").value,
+      cast: [],
+      genres: []
+    };
+
+    let movies = JSON.parse(localStorage.getItem("movies")) || [];
+    movies.push(movie);
+    localStorage.setItem("movies", JSON.stringify(movies));
+
+    window.location.href = "index.html";
+    if (localStorage.getItem("datalist")) {
+  data = JSON.parse(localStorage.getItem("datalist"));
+} else {
+  data = [];
+}
 
   });
